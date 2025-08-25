6ad50822c8f3cc9949de9b4c13487a23a8d898dc
PPjh123 <96333021+PPjh123@users.noreply.github.com>
2025-08-25 15:31:47 +0800

    Update data_tf.py

--- data_tf.py
+++ data_tf.py
@@ -2,7 +2,7 @@ import h5py
 import numpy as np
 from scipy.io import loadmat
 path='D:\DATA/Wave_height_super_resolution/Data/ERA5-3h-wt/'
-dataset=['S1_2021_2024','S2_2021_2024','S4_2021_2024']
+dataset=['S1_2021_2024','S2_2021_2024','S3_2021_2024']
 # 加载 .mat 文件
 for data in dataset:
     with h5py.File(path+data+'.mat') as f:  # 替换为你的文件名
