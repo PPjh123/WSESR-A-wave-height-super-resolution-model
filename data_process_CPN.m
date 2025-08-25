373001acbac9cd0b8dbb66241d7292cc7ba2f6a0
PPjh123 <96333021+PPjh123@users.noreply.github.com>
2025-08-25 15:30:55 +0800

    Update data_process_CPN.m

--- data_process_CPN.m
+++ data_process_CPN.m
@@ -1,23 +1,24 @@
 clear
 clc
 %data loading
-ncdisp('Copernicus-3h\S4-2021-2024.nc')
-data=ncread('Copernicus-3h\S4-2021-2024.nc','VHM0');%%%%%%%%
+ncdisp('Copernicus-3h\S3-2021-2024.nc')
+data=ncread('Copernicus-3h\S3-2021-2024.nc','VHM0');%%%%%%%%
 % area cut
 swh= permute(data, [2, 1, 3]);
 swh=flipud(swh);
 swh=swh(2:end,2:end,:);
-save Copernicus-3h\S4_2021_2024.mat swh%%%%%%%%
+save Copernicus-3h\S3_2021_2024.mat swh%%%%%%%%
 % % check
 % X=data(:,:,1);
 % imagesc(X)
 %% Nan value padding
 clear
 clc
-load 'Copernicus-3h\S4_2021_2024.mat'%%%%%%%%
+load 'Copernicus-3h\S3_2021_2024.mat'%%%%%%%%
 swh(isnan(swh)) = 0;
-save Copernicus-3h\S4_2021_2024.mat swh%%%%%%%%
+save Copernicus-3h\S3_2021_2024.mat swh%%%%%%%%
 %check
 X=swh(:,:,1);
 nan_value= any(isnan(X),'all');
+
 imagesc(X)
